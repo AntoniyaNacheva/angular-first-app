60886d2ad96e81451d9cad219b393b3f914edbdf
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-11-09 11:47:28 +0200

    Correction

--- Forum-app/src/app/api.service.ts
+++ Forum-app/src/app/api.service.ts
@@ -1,11 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClientModule) {}
+  constructor(private http: HttpClient) {}
 
   getThemes() {}
   getPosts() {}
