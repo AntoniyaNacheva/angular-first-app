a700a7dd4b5cf2b1a92d7827fe76dc470f9aad5b
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-11-09 12:35:29 +0200

    Interfaces

--- Forum-app/src/app/api.service.ts
+++ Forum-app/src/app/api.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/theme';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root',
@@ -7,6 +10,13 @@ import { Injectable } from '@angular/core';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  getThemes() {}
-  getPosts() {}
+  getThemes() {
+    const { appUrl } = environment;
+    return this.http.get<Theme[]>(`${appUrl}/themes`);
+  }
+  getPosts(limit?: number) {
+    const { appUrl } = environment;
+    const limitFilter = limit ? `?limit=${limit}` : '';
+    return this.http.get<Post[]>(`${appUrl}/posts${limitFilter}`);
+  }
 }
